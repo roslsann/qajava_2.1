b98223c9b7dc906f64572d9e2a08473310e04d60
roslsann <sann23@mail.ru>
2021-12-05 16:25:34 +0300

    main.java added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        int ticketprice = 15000;
+        int milecost = 20;
+        int bonus = ticketprice / milecost;
+        System.out.println(bonus + " бонусных миль начисленно");;
+
     }
 }
